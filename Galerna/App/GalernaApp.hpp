1611df766c92f1ee582762eb6faba3fd7b3643d0
cesar <cesargufer@gmail.com>
2026-07-09 22:12:12 +0000

    Refactor GalernaApp to use an array of status LEDs instead of a single LED

--- Galerna/App/GalernaApp.hpp
+++ Galerna/App/GalernaApp.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+
+#include <array>
+#include <cstdint>
+#include <functional>
 #include "Galerna/Hal/GpioConcept.hpp"
 
 namespace galerna::app
@@ -9,23 +13,34 @@ template <hal::Gpio TStatusLed>
 class GalernaApp
 {
 public:
-    explicit GalernaApp(TStatusLed& statusLed)
-        : statusLed_{statusLed}
+    explicit GalernaApp(std::array<std::reference_wrapper<TStatusLed>, 3> statusLeds)
+        : _statusLeds{statusLeds}
     {
     }
 
     void init()
     {
-        statusLed_.set(false);
+        for (auto& statusLed : _statusLeds)
+        {
+            statusLed.get().set(false);
+        }
     }
 
     void tick()
     {
-        statusLed_.set(!statusLed_.get());
+        for (std::size_t i = 0; i < _statusLeds.size(); ++i)
+        {
+            const bool value = static_cast<bool>((_currentStep >> i) & 0x1u);
+            _statusLeds[i].get().set(value);
+        }
+
+        _currentStep = (_currentStep + 1) & ((1u << _statusLeds.size()) - 1u);
     }
 
 private:
-    TStatusLed& statusLed_;
+    std::uint8_t _currentStep{0};
+    std::array<std::reference_wrapper<TStatusLed>, 3> _statusLeds;
+
 };
 
 } // namespace galerna::app
