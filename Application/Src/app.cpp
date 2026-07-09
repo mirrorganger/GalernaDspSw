ef5484ba6a8c5c110cc8535f1d4240bcbe34ae8b
cesar <cesargufer@gmail.com>
2026-07-09 22:12:00 +0000

    led pattern

--- Application/Src/app.cpp
+++ Application/Src/app.cpp
@@ -8,7 +8,17 @@
 namespace
 {
 
-galerna::platform::stm32f405::Stm32Gpio statusLed{LED_0_GPIO_Port, LED_0_Pin};
+galerna::platform::stm32f405::Stm32Gpio statusLed0{LED_0_GPIO_Port, LED_0_Pin};
+galerna::platform::stm32f405::Stm32Gpio statusLed1{LED_1_GPIO_Port, LED_1_Pin};
+galerna::platform::stm32f405::Stm32Gpio statusLed2{LED_2_GPIO_Port, LED_2_Pin};
+
+std::array<std::reference_wrapper<galerna::platform::stm32f405::Stm32Gpio>, 3> statusLed{
+    std::ref(statusLed0),
+    std::ref(statusLed1),
+    std::ref(statusLed2)
+};
+
+
 galerna::app::GalernaApp app{statusLed};
 
 } // namespace
